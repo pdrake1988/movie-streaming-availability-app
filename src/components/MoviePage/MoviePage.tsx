62796d3dc0c77318668e5d8d6bd8a8bfd0792d7e
pdrake1988 <pdrake1@me.com>
2021-12-07 07:11:06 -0600

    done

--- src/components/MoviePage/MoviePage.tsx
+++ src/components/MoviePage/MoviePage.tsx
@@ -55,104 +55,104 @@ export default function MoviePage(props: any) {
                     <h1 className={'text-center'}>Where to watch</h1>
                     <p className={'text-center'}>Data source Just Watch</p>
                 </div>
-                <div className={'col-2 p-1 m-1'}>
-                    <a className={'text-black align-content-center'} href={provider?.link}>The Moviedb movie page</a>
+                <div className={'col-2'}>
+                    <a className={'text-primary align-content-center'} href={provider?.link}>The Moviedb movie page</a>
                 </div>
-                <div className={'col-2 p-1 m-1'}>
+                <div className={'col-2'}>
                     <h1 className={'text-center d-none d-sm-none d-md-block'}>Flatrate</h1>
                     <h3 className={'text-center d-sm-block d-md-none'}>Flatrate</h3>
                     {provider?.flatrate !== undefined && provider?.flatrate.map((company: Providers, index: number) => {
                         return(
                             <React.Fragment key={index}>
-                                <img className={'img-fluid d-none d-md-block'} src={'https://image.tmdb.org/t/p/w92' + company.logo_path} alt={'Company Logo'}/>
-                                <img className={'img-fluid d-sm-block d-md-none'} src={'https://image.tmdb.org/t/p/w45' + company.logo_path} alt={'Company Logo'}/>
-                                <h3 className={'d-none d-sm-none d-md-block'}>{company.provider_name}</h3>
-                                <h6 className={'d-sm-block d-md-none'}>{company.provider_name}</h6>
+                                <img className={'img-fluid d-none d-md-block mx-auto'} src={'https://image.tmdb.org/t/p/w92' + company.logo_path} alt={'Company Logo'}/>
+                                <img className={'img-fluid d-sm-block d-md-none mx-auto'} src={'https://image.tmdb.org/t/p/w45' + company.logo_path} alt={'Company Logo'}/>
+                                <h3 className={'d-none d-sm-none d-md-block text-center'}>{company.provider_name}</h3>
+                                <h6 className={'d-sm-block d-md-none text-center'}>{company.provider_name}</h6>
                             </React.Fragment>
                         )
                     })}
                 </div>
-                <div className={'col-2 p-1 m-1'}>
+                <div className={'col-2'}>
                     <h1 className={'text-center d-none d-sm-none d-md-block'}>Buy</h1>
                     <h3 className={'text-center d-sm-block d-md-none'}>Buy</h3>
                     {provider?.buy !== undefined && provider?.buy.map((company: Providers, index: number) => {
                         return(
                             <React.Fragment key={index}>
-                                <img className={'img-fluid d-none d-md-block'} src={'https://image.tmdb.org/t/p/w92' + company.logo_path} alt={'Company Logo'}/>
-                                <img className={'img-fluid d-sm-block d-md-none'} src={'https://image.tmdb.org/t/p/w45' + company.logo_path} alt={'Company Logo'}/>
-                                <h3 className={'d-none d-sm-none d-md-block'}>{company.provider_name}</h3>
-                                <h6 className={'d-sm-block d-md-none'}>{company.provider_name}</h6>
+                                <img className={'img-fluid d-none d-md-block mx-auto'} src={'https://image.tmdb.org/t/p/w92' + company.logo_path} alt={'Company Logo'}/>
+                                <img className={'img-fluid d-sm-block d-md-none mx-auto'} src={'https://image.tmdb.org/t/p/w45' + company.logo_path} alt={'Company Logo'}/>
+                                <h3 className={'d-none d-sm-none d-md-block text-center'}>{company.provider_name}</h3>
+                                <h6 className={'d-sm-block d-md-none text-center'}>{company.provider_name}</h6>
                             </React.Fragment>
                         )
                     })}
                 </div>
-                <div className={'col-2 p-1 m-1'}>
+                <div className={'col-2'}>
                     <h1 className={'text-center d-none d-sm-none d-md-block'}>For Rent</h1>
                     <h3 className={'text-center d-sm-block d-md-none'}>For Rent</h3>
                     {provider?.rent !== undefined && provider?.rent.map((company: Providers, index: number) => {
                         return(
                             <React.Fragment key={index}>
-                                <img className={'img-fluid d-none d-md-block'} src={'https://image.tmdb.org/t/p/w92' + company.logo_path} alt={'Company Logo'}/>
-                                <img className={'img-fluid d-sm-block d-md-none'} src={'https://image.tmdb.org/t/p/w45' + company.logo_path} alt={'Company Logo'}/>
-                                <h3 className={'d-none d-sm-none d-md-block'}>{company.provider_name}</h3>
-                                <h6 className={'d-sm-block d-md-none'}>{company.provider_name}</h6>
+                                <img className={'img-fluid d-none d-md-block mx-auto'} src={'https://image.tmdb.org/t/p/w92' + company.logo_path} alt={'Company Logo'}/>
+                                <img className={'img-fluid d-sm-block d-md-none mx-auto'} src={'https://image.tmdb.org/t/p/w45' + company.logo_path} alt={'Company Logo'}/>
+                                <h3 className={'d-none d-sm-none d-md-block text-center'}>{company.provider_name}</h3>
+                                <h6 className={'d-sm-block d-md-none text-center'}>{company.provider_name}</h6>
                             </React.Fragment>
                         )
                     })}
                 </div>
-                <div className={'col-2 p-1 m-1'}>
+                <div className={'col-2'}>
                     <h1 className={'text-center d-none d-sm-none d-md-block'}>Free with Ads</h1>
                     <h3 className={'text-center d-sm-block d-md-none'}>Free with Ads</h3>
                     {provider?.ads !== undefined && provider?.ads.map((company: Providers, index: number) => {
                         return(
                             <React.Fragment key={index}>
-                                <img className={'img-fluid d-none d-md-block'} src={'https://image.tmdb.org/t/p/w92' + company.logo_path} alt={'Company Logo'}/>
-                                <img className={'img-fluid d-sm-block d-md-none'} src={'https://image.tmdb.org/t/p/w45' + company.logo_path} alt={'Company Logo'}/>
-                                <h3 className={'d-sm-none d-md-block'}>{company.provider_name}</h3>
-                                <h6 className={'d-none d-sm-block d-md-none'}>{company.provider_name}</h6>
+                                <img className={'img-fluid d-none d-md-block mx-auto'} src={'https://image.tmdb.org/t/p/w92' + company.logo_path} alt={'Company Logo'}/>
+                                <img className={'img-fluid d-sm-block d-md-none mx-auto'} src={'https://image.tmdb.org/t/p/w45' + company.logo_path} alt={'Company Logo'}/>
+                                <h3 className={'d-sm-none d-md-block text-center'}>{company.provider_name}</h3>
+                                <h6 className={'d-none d-sm-block d-md-none text-center'}>{company.provider_name}</h6>
                             </React.Fragment>
                         )
                     })}
                 </div>
-                <div className={'col-2 p-1 m-1'}>
+                <div className={'col-2'}>
                     <h1 className={'text-center d-none d-sm-none d-md-block'}>Free</h1>
                     <h3 className={'text-center d-sm-block d-md-none'}>Free</h3>
                     {provider?.free !== undefined && provider?.free.map((company: Providers, index: number) => {
                         return(
                             <React.Fragment key={index}>
-                                <img className={'img-fluid d-none d-md-block'} src={'https://image.tmdb.org/t/p/w92' + company.logo_path} alt={'Company Logo'}/>
-                                <img className={'img-fluid d-sm-block d-md-none'} src={'https://image.tmdb.org/t/p/w45' + company.logo_path} alt={'Company Logo'}/>
-                                <h3 className={'d-none d-sm-none d-md-block'}>{company.provider_name}</h3>
-                                <h6 className={'d-sm-block d-md-none'}>{company.provider_name}</h6>
+                                <img className={'img-fluid d-none d-md-block mx-auto'} src={'https://image.tmdb.org/t/p/w92' + company.logo_path} alt={'Company Logo'}/>
+                                <img className={'img-fluid d-sm-block d-md-none mx-auto'} src={'https://image.tmdb.org/t/p/w45' + company.logo_path} alt={'Company Logo'}/>
+                                <h3 className={'d-none d-sm-none d-md-block text-center'}>{company.provider_name}</h3>
+                                <h6 className={'d-sm-block d-md-none text-center'}>{company.provider_name}</h6>
                             </React.Fragment>
                         )
                     })}
                 </div>
-                <div className={'col-4 p-1 m-1'}>
+                <div className={'col-4'}>
                     <h4 className={'text-center'}>Production Companies</h4>
                     {movie?.production_companies.map((company: ProductionCompanies, index) => {
                         return(
-                            <React.Fragment key={index}>
-                                <img className={'img-fluid d-none d-md-block'} src={'https://image.tmdb.org/t/p/w92' + company.logo_path} alt={'Company Logo'}/>
-                                <img className={'img-fluid d-sm-block d-md-none'} src={'https://image.tmdb.org/t/p/w45' + company.logo_path} alt={'Company Logo'}/>
-                                <p>{company.name}</p>
-                            </React.Fragment>
+                            <div key={index}>
+                                <img className={'img-fluid d-none d-md-block mx-auto'} src={'https://image.tmdb.org/t/p/w92' + company.logo_path} alt={'Company Logo'}/>
+                                <img className={'img-fluid d-sm-block d-md-none mx-auto'} src={'https://image.tmdb.org/t/p/w45' + company.logo_path} alt={'Company Logo'}/>
+                                <p className={'text-center'}>{company.name}</p>
+                            </div>
                         )
                     })}
                 </div>
-                <div className={'col-4 p-1 m-1'}>
+                <div className={'col-4'}>
                     <h4 className={'text-center'}>Production Countries</h4>
                     {movie?.production_countries.map((country: ProductionCountries, index: number) => {
                         return (
-                            <p key={index}>{country.name}</p>
+                            <p className={'text-center'} key={index}>{country.name}</p>
                         )
                     })}
                 </div>
-                <div className={'col-4 p-1 m-1'}>
+                <div className={'col-4'}>
                     <h4 className={'text-center'}>Spoken Languages</h4>
                     {movie?.spoken_languages.map((language: SpokenLanguages, index) => {
                         return(
-                                <p key={index}>{language.english_name}</p>
+                                <p className={'text-center'} key={index}>{language.english_name}</p>
                         )
                     })}
                 </div>
